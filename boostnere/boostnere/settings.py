cb838e7314ea7181481e99b9621da7fb1c44031d
Varo1234 <51148348+Varo1234@users.noreply.github.com>
2023-10-16 15:09:27 -0600

    Cambio en cors headers

--- boostnere/boostnere/settings.py
+++ boostnere/boostnere/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'home',
     'projects',
     'login_register',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -120,8 +122,11 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
